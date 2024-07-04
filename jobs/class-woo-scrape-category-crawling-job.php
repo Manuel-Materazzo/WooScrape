5395d94d9cc8874f78916eb0c5ca835af48b5649
Manuel <manuelmaterazzo@gmail.com>
2024-07-04 14:38:02 +0200

    individual product crawling

--- jobs/class-woo-scrape-category-crawling-job.php
+++ jobs/class-woo-scrape-category-crawling-job.php
@@ -1,21 +1,22 @@
 <?php
+
 require ABSPATH . 'wp-content/plugins/woo-scrape/vendor/simple_html_dom.php';
 require ABSPATH . 'wp-content/plugins/woo-scrape/services/class-woo-scrape-fishdeal-crawler-service.php';
 
 class Woo_scrape_category_crawling_job {
 	private static Woo_scrape_fishdeal_crawler_service $crawler;
+	private static string $date_format = 'Y-m-d H:i:s';
 
 	function __construct() {
 		self::$crawler = new Woo_scrape_fishdeal_crawler_service();
 	}
 
 	public function run() {
-		global $wpdb;
-
+		// crawl categories to get partial products
 		$this->fetch_categories_for_profitable_products();
 
-			error_log( "The category " . $category->id . " has crawled " . count( $profitable_products ) . " items" );
-
+		// crawl each partial product to complete informations
+		$this->fetch_profitable_products();
 
 
 			// save the new products
@@ -54,7 +55,171 @@ class Woo_scrape_category_crawling_job {
 		}
 	}
 
+	/**
+	 * Gets today's profitable products, and updates detailed metadata of each product
+	 * @return void
+	 */
+	private function fetch_profitable_products(): void {
+		global $wpdb;
 
+		$page                = 0;
+		$products_table_name = $wpdb->prefix . 'woo_scrape_products';
+		$now                 = date( self::$date_format );
+
+		// gets profitable products crawled today. Does not crawl products that have has_variants = false
+		// (already crawled once, and found no variants. using the categpry price is fine)
+		while ( true ) {
+			// get product page
+			$start            = $page * 30;
+			$updated_products = $wpdb->get_results(
+				"SELECT id, url, image_urls, image_ids FROM $products_table_name
+                WHERE DATE(`latest_crawl_timestamp`) = CURDATE()
+                and latest_crawl_timestamp has_variations is not false
+                LIMIT $start,30"
+			);
+
+			error_log( "Fetched " . count( $updated_products ) . " products to crawl and update." );
+
+			// if there are no product left to crawl, stop the cycle
+			if ( empty( $updated_products ) ) {
+				error_log( "There are no more updated products" );
+				break;
+			}
+
+			// crawl each product to get the complete informations
+			foreach ( $updated_products as $partial_product ) {
+				$complete_product = self::$crawler->crawl_product( $partial_product->url );
+
+				error_log( "Crawled " . $partial_product->url );
+
+				// if the product has no crawled images, or the images changed
+				if ( ! $partial_product->image_ids ||
+				     json_encode( $complete_product->getImageUrls() ) !== $partial_product->image_urls ) {
+					error_log( "Found new images for " . $partial_product->url . " : " . json_encode( $complete_product->getImageUrls() ) );
+					// crawl images and save them
+					$image_ids = self::$crawler->crawl_images( $complete_product->getImageUrls() );
+					// add ids to the product
+					$complete_product->setImageIds( $image_ids );
+					error_log( "Crawled " . count($image_ids) . " images for " . $partial_product->url );
+				}
+
+				// update the product on DB
+				$this->update_complete_product( $now, $partial_product->id, $complete_product );
+
+				if ( $complete_product->hasvariations() ) {
+					error_log( "The item " . $partial_product->url . "has " . count($complete_product->getVariations()) . " variations!");
+					// update variations on DB
+					$new_variations = $this->update_variations( $now, $partial_product->id, $complete_product->getVariations() );
+					error_log( "The item " . $partial_product->url . "has " . count($new_variations) . " new variations!");
+					// create new variations on db
+					$this->save_variations( $now, $partial_product->id, $new_variations );
+				}
+
+			}
+			$page += 1;
+		}
+	}
+
+	/**
+	 * Updates on DB partial products to enrich and complete their metadata.
+	 *
+	 * @param string $now the product update timestamp
+	 * @param int $product_id the DB product id
+	 * @param WooScrapeProduct $complete_product the complete product object
+	 *
+	 * @return void
+	 */
+	private function update_complete_product( string $now, int $product_id, WooScrapeProduct $complete_product ): void {
+		global $wpdb;
+
+		// update products with the additional details
+		$rows_updated = $wpdb->update(
+			$wpdb->prefix . 'woo_scrape_products',
+			array(
+				'item_updated_timestamp' => $now,
+				'image_urls'             => json_encode( $complete_product->getImageUrls() ),
+				'image_ids'              => json_encode( $complete_product->getImageIds() ),
+				'description'            => $complete_product->getDescription()
+			),
+			array( 'id' => $product_id )
+		);
+
+	}
+
+	/**
+	 * Updates on DB the variations of the $variations array, and returns the ones not found on DB.
+	 *
+	 * @param string $now the variation update timestamp
+	 * @param int $product_id the id of the variation's base product
+	 * @param array $variations array of product variations
+	 *
+	 * @return array array of variations not updated
+	 */
+	private function update_variations( string $now, int $product_id, array $variations ): array {
+		global $wpdb;
+
+		foreach ( $variations as $key => $variation ) {
+			// update variation already on table
+			$rows_updated = $wpdb->update(
+				$wpdb->prefix . 'woo_scrape_products',
+				array(
+
+//					'quantity'                   => $variation->getName(),
+					'latest_crawl_timestamp' => $now,
+					'item_updated_timestamp' => $now,
+//					'suggested_price'        => strval( $variation->getSuggestedPrice() ),
+					'discounted_price'       => strval( $variation->getDiscountedPrice() ),
+				),
+				array(
+					'product_id' => $product_id,
+					'name'       => $variation->getName(),
+				)
+			);
+
+			// if the the product got updated, remove it from the array
+			if ( $rows_updated >= 1 ) {
+				unset( $variations[ $key ] );
+			}
+			// there shouldn't be more than one row updated
+			if ( $rows_updated > 1 ) {
+				error_log(
+					"There is more than one variation on database for the product" . $product_id . "  with name "
+					. $variation->getName()
+				);
+			}
+		}
+
+		// returns variations that didn't update
+		return $variations;
+	}
+
+	/**
+	 * Creates on DB the variations of the $variations array.
+	 *
+	 * @param string $now the variation update timestamp
+	 * @param int $product_id the id of the variation's base product
+	 * @param array $variations array of product variations
+	 *
+	 */
+	private function save_variations( string $now, int $product_id, array $variations ): void {
+		global $wpdb;
+
+		foreach ( $variations as $variation ) {
+			// create the variation on db
+			$wpdb->insert(
+				$wpdb->prefix . 'woo_scrape_products',
+				array(
+					'name'                   => $variation->getName(),
+					'product_id'             => $product_id,
+					'first_crawl_timestamp'  => $now,
+					'latest_crawl_timestamp' => $now,
+					'item_updated_timestamp' => $now,
+//					'suggested_price'        => strval( $profitable_product->getSuggestedPrice() ),
+					'discounted_price'       => strval( $variation->getDiscountedPrice() ),
+				)
+			);
+		}
+	}
 
 	/**
 	 * Updates on DB the products of the $profitable_products array, and returns the ones not found on DB.
@@ -68,7 +233,7 @@ class Woo_scrape_category_crawling_job {
 	private function update_partial_products( array $partial_products ): array {
 		global $wpdb;
 
-		$now = date( 'Y-m-d H:i:s' );
+		$now = date( self::$date_format );
 
 		foreach ( $partial_products as $key => $partial_product ) {
 			// update products already on the table
@@ -119,10 +284,10 @@ class Woo_scrape_category_crawling_job {
 	 *
 	 * @param array $partial_products array of products to insert
 	 */
-	private function save_partial_products( array $partial_products ) {
+	private function save_partial_products( array $partial_products ): void {
 		global $wpdb;
 
-		$now = date( 'Y-m-d H:i:s' );
+		$now = date( self::$date_format );
 
 		foreach ( $partial_products as $partial_product ) {
 			$wpdb->insert(
