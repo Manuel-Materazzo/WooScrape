b29ee9319247b306fac53348e6b640a9bfc95905
Manuel <manuelmaterazzo@gmail.com>
2024-07-30 18:59:49 +0200

    image name randomization

--- services/abstracts/class-woo-scrape-abstract-crawler-service.php
+++ services/abstracts/class-woo-scrape-abstract-crawler-service.php
@@ -21,8 +21,7 @@ abstract class Woo_Scrape_Abstract_Crawler_Service
             // generate file name
             $exploded  = explode( '/', getimagesize( $url )['mime'] );
             $imagetype = end( $exploded );
-            $uniq_name = date( 'dmY' ) . '' . (int) microtime( true );
-            $filename  = $uniq_name . '.' . $imagetype;
+            $filename  = uniqid($this->guidv4()) . '.' . $imagetype;
 
 			// free up memory
 	        unset($exploded);
@@ -120,4 +119,25 @@ abstract class Woo_Scrape_Abstract_Crawler_Service
 	    throw new Exception("The request failed after {$max_retries} attempts");
 
     }
+
+	/**
+	 * Generates a random UUIDv4
+	 * @param $data
+	 *
+	 * @return string|void
+	 * @throws \Random\RandomException
+	 */
+	private function guidv4($data = null) {
+		// Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+		$data = $data ?? random_bytes(16);
+		assert(strlen($data) == 16);
+
+		// Set version to 0100
+		$data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+		// Set bits 6-7 to 10
+		$data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+		// Output the 36 character UUID.
+		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+	}
 }
