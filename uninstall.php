dc46534e4b9aaf3a9f9745158f4294ce03134dd0
Manuel <manuelmaterazzo@gmail.com>
2026-03-07 00:13:28 +0100

    Feat: Implement uninstall.php that drops all custom tables, deletes all woo_scrape_* options, and clears scheduled hooks

--- uninstall.php
+++ uninstall.php
@@ -3,22 +3,6 @@
 /**
  * Fired when the plugin is uninstalled.
  *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
  * @link       https://github.com/Manuel-Materazzo
  * @since      1.0.0
  *
@@ -29,3 +13,17 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+global $wpdb;
+
+// Drop custom tables
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}woo_scrape_variations" );
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}woo_scrape_products" );
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}woo_scrape_pages" );
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}woo_scrape_job_logs" );
+
+// Delete all plugin options
+$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE 'woo_scrape_%'" );
+
+// Clear scheduled hooks
+wp_clear_scheduled_hook( 'woo_scrape_orchestration_job_hook' );
